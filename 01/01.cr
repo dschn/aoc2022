fcc824f42141ec591cb1db52501d5db30e15d504
Dustin Schneider <dschneider@doximity.com>
2022-12-02 11:41:00 -0600

    Add day 1

---
+++ 01/01.cr
@@ -0,0 +1,8 @@
+elves_input = File.read_lines("input")
+elves_calories = [] of Int64
+
+elves_input.chunk { |x| x == "" }.reject { |bool,_| bool == true }.each do |bool, calories|
+  elves_calories << calories.map { |value| value.to_i }.sum
+end
+
+puts elves_calories.max
